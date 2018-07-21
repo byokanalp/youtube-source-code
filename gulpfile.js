7ede0a76b068c4f9c9e06494ef4035ffeb0201d3
byokanalp <okan-alp-94@windowslive.com>
2018-07-22 00:35:29 +0300

    Projeye gulp ve browser-sync eklendi. Bazı dosyaların isimleri değiştirildi. Projenin genel iskelet yapısı oluşturuldu..

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp        = require('gulp'),
+    browserSync = require('browser-sync')
+
+let PATH = {
+    root           : 'projects/_TRIALS/',
+    currentProject : 'hc-basit-buton-yapimi'
+}
+
+gulp.task('browser-sync', () => {
+    browserSync.init({
+        browser: 'opera',
+        notify: false,
+        server: {
+            baseDir: './' + PATH.root + PATH.currentProject,
+            proxy: '127.0.0.1:8000',
+        }
+    })
+})
+
+
+gulp.task('watch', () => {
+    gulp.watch(PATH.root + PATH.currentProject + '**/*.html').on('change', () => { 
+        browserSync.reload() 
+    })
+    gulp.watch(PATH.root + PATH.currentProject + '**/*.css').on('change', () => { 
+        browserSync.reload() 
+    })
+    gulp.watch(PATH.root + PATH.currentProject + '**/*.js').on('change', () => { 
+        browserSync.reload() 
+    })
+})
+
+
+gulp.task('default', ['browser-sync' , 'watch'])
